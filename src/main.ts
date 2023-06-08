a1dbfdce3006d542a835caed94faf86aba68cd19
Artem Balanovskyi <mr.balanovsky@gmail.com>
2023-06-08 11:38:54 +0300

    fix: add cors origin to options

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import * as process from "process";
 
 async function bootstrap() {
-  try {
-    const app = await NestFactory.create(AppModule, {cors: true});
-    await app.listen(process.env.PORT || 5000, () => console.log(`Server was started on PORT ${process.env.PORT || 5000}`));
-  } catch (err) {
-    console.log(`Bootstrap error: ${err.message}`)
-  }
+    try {
+
+        const app = await NestFactory.create(AppModule);
+
+        app.enableCors({origin: process.env.CLIENT_APP_URL});
+
+        await app.listen(process.env.PORT || 5000, () => console.log(`Server was started on PORT ${process.env.PORT || 5000}`));
+    } catch (err) {
+        console.log(`Bootstrap error: ${err.message}`)
+    }
 
 }
 
